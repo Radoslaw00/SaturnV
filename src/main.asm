e268bd452fb0c94e03513be4c50f483f52c2bca9
Radosław H <holubowskir@gmail.com>
2025-12-16 21:24:04 +0100

    main function in src started

---
+++ src/main.asm
@@ -0,0 +1,14 @@
+section .text
+global _start   ; entry point
+
+_start:
+.loop:
+    ; call functions here
+    ; call my function
+
+    jmp .loop     ; infinite loop
+
+; exit (never reached in this infinite loop)
+mov eax, 1        ; sys_exit
+mov ebx, 0        ; return code 0
+int 0x80
